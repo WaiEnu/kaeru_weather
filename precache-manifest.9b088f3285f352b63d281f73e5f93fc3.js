045fe78ada57f0477231908c5d25974fe39ef304
WaiEnu <strayhituj@gmail.com>
2020-06-25 20:33:29 +0900

    Updates

---
+++ precache-manifest.9b088f3285f352b63d281f73e5f93fc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb45e7d61c0264323385b53a7db8627f",
+    "url": "/KaeruWeather/index.html"
+  },
+  {
+    "revision": "1bf7d3b43e2a6e9ae6f6",
+    "url": "/KaeruWeather/static/css/main.489ac10b.chunk.css"
+  },
+  {
+    "revision": "4d03a5508b0da2083f24",
+    "url": "/KaeruWeather/static/js/2.896b471e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/KaeruWeather/static/js/2.896b471e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bf7d3b43e2a6e9ae6f6",
+    "url": "/KaeruWeather/static/js/main.6d94ec26.chunk.js"
+  },
+  {
+    "revision": "0694d521625fd13e3a8b",
+    "url": "/KaeruWeather/static/js/runtime-main.32751fa0.js"
+  }
+]);
